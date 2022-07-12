54a3125ca82ed2fc3c7f4e541f45e6964913dbd1
CompuTibo <southiratn.thibaud@gmail.com>
2022-07-12 22:41:50 +0200

    Avancée encore

--- Test/sendAccepted.js
+++ Test/sendAccepted.js
@@ -8,8 +8,17 @@ const {
 const {
     DeviceCodeCredential
 } = require("@azure/identity");
+/* const msal = require('@azure/msal-node');
+const msalConfig = {
+  auth: {
+      clientId: "e0bcb58f-e00a-45b0-bc15-5905df9f5654",
+      authority: "https://login.microsoftonline.com/d1beaacb-c6b5-42b9-8869-2810fb362440",
+      clientSecret: "l3l8Q~iXflF0IKA.VHKKrudTRVbyc48qf9G8gbKf",
+ }
+};
+const cca = new msal.ConfidentialClientApplication(msalConfig); */
 
-const credential = new DeviceCodeCredential('d1beaacb-c6b5-42b9-8869-2810fb362440', 'e0bcb58f-e00a-45b0-bc15-5905df9f5654', '733e91ea-8dab-4328-8fef-8ae080d1cdff');
+const credential = new DeviceCodeCredential('d1beaacb-c6b5-42b9-8869-2810fb362440', 'e0bcb58f-e00a-45b0-bc15-5905df9f5654', 'l3l8Q~iXflF0IKA.VHKKrudTRVbyc48qf9G8gbKf');
 const authProvider = new TokenCredentialAuthenticationProvider(credential, {
     scopes: ["user.read", "mail.send"]
 });
